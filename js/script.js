3ef2ad3bf356a34cbe2224e5ef6c72beaccae797
Francildo Brito <96621544+FrancildoBrito@users.noreply.github.com>
2022-02-28 10:28:30 -0300

    post

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const _elements = {
+	date: document.querySelector(".date"),
+
+	scrollLinks: document.querySelectorAll(".navbar-list__link, .footer-list__link"),
+	navbarList: document.querySelector(".navbar-list"),
+	toggle: document.querySelector(".navbar-header__toggle")}
+
+let _sliderCounter = 0, _touchStart, _touchEnd;
+
+_elements.date.innerHTML = new Date().getFullYear() + "."
+
+_elements.scrollLinks.forEach(link => {
+	link.addEventListener("click", e =>{
+		_elements.navbarList.classList.remove("navbar-list--show-links")
+		const id = link.getAttribute("href");
+		const element = document.querySelector(id);
+
+		const position = element.offsetTop - 62;
+
+		window.scrollTo({
+			top: position,
+			behavior: "smooth"
+		});
+
+		e.preventDefault();
+	});
+});
+
+_elements.toggle.addEventListener("click", () => {
+	_elements.navbarList.classList.toggle("navbar-list--show-links")
+
+});
+
+_elements.galleryItems.forEach(item => {
+      item.addEventListener("click", e => {
+		  const id = getImageId(e.target);
+		  updateModal(id);
+          _elements.modal.style.display = "flex";
+	  });
+});
+
